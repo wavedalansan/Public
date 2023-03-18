1748b09b8c98bb4a3d386eada6f8a1aeffcf2bc2
wavedalansan <126333164+wavedalansan@users.noreply.github.com>
2023-03-18 13:38:14 +0800

    Update 关闭进程.py

--- 关闭进程.py
+++ 关闭进程.py
@@ -17,13 +17,13 @@ def close_jy(*args):
         tkinter.messagebox.showinfo('Information','进程已尝试关闭!')
 
 def admin_mode():
-    os.startfile("admin.py")
+    os.startfile("admin.py")#admin.py放于该文件的相同文件夹下
 
 text_tip = tkinter.Label(root,text='请输入进程名:',fg='blue').place(x=0,y=0)
 text = tkinter.Label(root,text='按下按钮以关闭进程').place(x=90,y=60)
 text_warn = tkinter.Label(root,text='输入进程名称且必须要带上后缀名!',fg='red').place(x=50,y=200)
 close_button = tkinter.Button(root,text='关闭进程',command=close_jy).place(x=120,y=120)
-administrator_button = tkinter.Button(root,text='管理员模式',fg='green').place(x=220,y=260)
+administrator_button = tkinter.Button(root,text='管理员模式',fg='green',command=admin_mode).place(x=220,y=260)
 close_pid = tkinter.Entry(root, width=50)
 close_pid.place(x=80, y=0, width=220, height=20)
 
